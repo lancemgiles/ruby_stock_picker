c3e6f9f0a56385b04dd4b412cb2e724794f689a6
Lance <dogmandevices@gmail.com>
2024-04-04 19:20:21 -0500

    running more tests shows that greatest profit is no tested just largest numbers

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,38 +1,24 @@
 def stock_picker(prices)
-	# for each price, find the highest number
-	# see if there is an price preceding the highest one
-	# if so, compare the difference between the highest price
-	# and the price before with the second highest price
-
-	# find the highest number
 	best_selling_price = prices.max
 	included_prices = prices
-	# is it the first element?
-	if best_selling_price == prices[0]
+	while best_selling_price == prices[0]
 		included_prices = prices.drop(1)
+		p included_prices
 		best_selling_price = included_prices.max
 	end
 	sell_date = prices.rindex(best_selling_price)
 	
-
 	best_buying_price = prices.min
-	if best_buying_price == prices[-1]
+	while best_buying_price == prices[-1]
 		included_prices.pop
 		best_buying_price = prices.min
 	end
 	sell_range = included_prices.slice(0, sell_date)
-	# if prices.index(best_buying_price) > sell_range[-1]
-	# 	included_prices.delete(best_buying_price)
-	# 	best_buying_price = included_prices.min
-	# end
-
-	
 	buy = included_prices.select{|date|
 								date == sell_range.min}
 	buy_date = prices.index(buy.join.to_i)
 	return [buy_date, sell_date]
-	
 end
 
 
-p stock_picker([1,3,6,9,20,8,6,10,22])
+p stock_picker([30,40,6,9,20,8,6,10,22])
