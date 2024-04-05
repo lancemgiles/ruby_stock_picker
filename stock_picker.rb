17e13c8f1e902166adc62fc800f1b49d890b1f5c
Lance <dogmandevices@gmail.com>
2024-04-04 20:19:17 -0500

    completely replaced old method as it only worked if largest number meant largest profit. replaced using ranges to execute based on profit

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,24 +1,22 @@
-def stock_picker(prices)
-	best_selling_price = prices.max
-	included_prices = prices
-	while best_selling_price == prices[0]
-		included_prices = prices.drop(1)
-		p included_prices
-		best_selling_price = included_prices.max
-	end
-	sell_date = prices.rindex(best_selling_price)
-	
-	best_buying_price = prices.min
-	while best_buying_price == prices[-1]
-		included_prices.pop
-		best_buying_price = prices.min
-	end
-	sell_range = included_prices.slice(0, sell_date)
-	buy = included_prices.select{|date|
-								date == sell_range.min}
-	buy_date = prices.index(buy.join.to_i)
-	return [buy_date, sell_date]
-end
+def stock_picker (prices)
+  max_profit= 0
+  buy_day= 0
+  sell_day= 1
+  buying_range = 0..prices.length - 1 #cannot buy on last day
 
+  buying_range.each do |bp|
+    (bp + 1..prices.length - 1).each do |sp| #selling range
+    #iterate through each day; setting selling range as a variable
+    #means range cannot be stepped through
+      current_profit = prices[sp]-prices[bp]
+      if current_profit > max_profit
+  	   	max_profit = current_profit
+        buy_day = bp
+        sell_day = sp    
+      end
+    end
+ end
+ return [buy_day, sell_day]
+end
 
 p stock_picker([30,40,6,9,20,8,6,10,22])
